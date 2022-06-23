283574ae6b3a9f7e2b9ae2ed4a989efe34b55371
qingxuanying <597707165@qq.com>
2022-06-23 10:54:24 +0800

    init

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './login.css'
+
+export default class Login extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>login!</Text>
+      </View>
+    )
+  }
+}
