7d889c108a8241f7364c2b41d3d4656e3eb266d3
qingxuanying <597707165@qq.com>
2022-06-27 20:31:19 +0800

    6.27

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -3,13 +3,41 @@ import React, { Component } from 'react'
 import { View, Text, Input, Image, Icon } from '@tarojs/components'
 import img_search from './img/search.png'
 import icon from './img/icon.png'
+import img1 from './img/img1.png'
+import img2 from './img/img2.png'
+import img3 from './img/img3.png'
 import './index.css'
 import Items from '../items/items'
 
 export default class Index extends Component {
-
+  constructor(props){
+    super(props)
+    this.state={
+      date:[
+        {
+          id:'01',
+          name:'123',
+          price:'321',
+          img:'1',
+          bathroom:'1',
+          bed:'2',
+          star:'3.0'
+        },
+        {
+          id:'02',
+          name:'123s',
+          price:'321d',
+          img:'1a',
+          bathroom:'4',
+          bed:'1',
+          star:'5.0'
+        }
+      ]
+    }
+  }
 
   render() {
+    let data=this.state.data
     return (
       <View className='index'>
 
@@ -25,29 +53,29 @@ export default class Index extends Component {
           </View>
         </View>
 
-        {/* <View className='lunbotu'>
-
-        </View> */}
-
-
-        {/* 
-        //废案
-        //
-        <View className='select'>
-          <View className='selections'>
-            <View className='selection'>所有</View>
-            <View className='selection'>居家</View>
-            <View className='selection'>办公</View>
-          </View>
-        </View> */}
-
-
         <View className='recommendation'>
           <Text className='re_text'>为你推荐</Text>
         </View>
+
         <View className='index_Items'>
-          <Items />
+        {this.state.date.map((dateobj) => { return <Items key={dateobj.id} {...dateobj} ></Items> })}
         </View>
+
+        <View className='footer'>
+          <View className='daohang'>
+            <Image className='daohang_img' src={img1}></Image>
+            <Text>首页</Text>
+          </View>
+          <View className='daohang'>
+            <Image className='daohang_img' src={img2}></Image>
+            <Text>房屋委托</Text>
+          </View>
+          <View className='daohang'>
+            <Image className='daohang_img' src={img3}></Image>
+            <Text>个人中心</Text>
+          </View>
+        </View>
+
 
 
       </View>
--- src/pages/items/items.jsx
+++ src/pages/items/items.jsx
@@ -1,24 +1,48 @@
+/* eslint-disable react/no-unused-state */
+
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react'
 import { View, Text, Input, Image, Icon } from '@tarojs/components'
 import './items.css'
 import img1 from './img/1.png'
+import bed from './img/bed.png'
+import bathroom from './img/bathroom.png'
+import star from './img/star.png'
 
 export default class Items extends Component {
-
+  constructor(props){
+    super(props)
+  }
 
   render() {
     return (
+      <View className='body'>
       <View className='item'>
 
         <View className='leftItem'>
             <Image src={img1} className='leftimg'></Image>
         </View>
         <View className='rightItem'>
+          <View className='name'>{this.props.name}</View>
+          <View className='price'>{this.props.price}元/天</View>
+          <View className='bott'>
 
+            <View className='bed'>
+              <Image className='icon_items' src={bed}></Image>
+              <Text>{this.props.bed}</Text>
+            </View>
+            <View className='bathroom' >
+              <Image className='icon_items' src={bathroom}></Image>
+              <Text>{this.props.bathroom}</Text>
+            </View>
+            <View className='star'>
+              <Image className='icon_items' src={star}></Image>
+              <Text>{this.props.star}</Text>
+            </View>
+
+          </View>
         </View>
-
-
+      </View>
       </View>
     )
   }
