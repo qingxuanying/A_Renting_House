49ac8dc5295a53361d941ca07baee3d0b0f0573a
qingxuanying <597707165@qq.com>
2022-06-27 12:04:49 +0800

    login_regist

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,65 +1,87 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react'
-import { View, Text, Input } from '@tarojs/components'
+import { View, Text, Input,Image } from '@tarojs/components'
 import Taro from '@tarojs/taro'
 import './login.css'
+import bc from './img/bc.jpg'
 
 export default class Login extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      user: 'dwadad',
-      pwd: 'dada'
+      user: '',
+      pwd: ''
     }
   }
 
   toRegist = () => {
     Taro.navigateTo({ url: '/pages/regist/regist' })
   }
-  changeUser(e){
+  changeUser=(e)=> {
     console.log(e)
     this.setState({
       user: e.target.value
     })
   }
-  changePwd(e){
+  changePwd =(e)=> {
     console.log(e.target)
     this.setState({
       pwd: e.target.value
     })
   }
-  login = ()=>{
-// 
-// 等待service接口
-// 
-//     
+  login = () => {
+    let user=this.state.user;
+    let pwd=this.state.pwd;
+    // 
+    // 等待service接口
+    // 
+    //     
     console.log(this.state.user)
     console.log(this.state.pwd)
+    Taro.navigateTo({
+      url:'/pages/index/index'
+    })
   }
 
 
   render() {
     return (
       <View className='index'>
-
         <View className='container'>
 
-          <View className='userinput'>
-            <Input className='input' placeholder='请输入账号' type='text' onInput={this.changeUser.bind(this)}></Input>
-          </View>
+          <View className='centercontainer'>
 
-          <View className='pwdinput'>
-            <Input className='input' placeholder='请输入密码' type='password' onInput={this.changePwd.bind(this)}></Input>
-          </View>
+            <View className='inputline'>
 
-          <View className='botton' onClick={this.login}>
-            <Text>登录</Text>
-          </View>
-          <View className='botton' onClick={this.toRegist}>
-            <Text>未有账号,前往注册</Text>
+              <Text className='text'>用户名</Text>
+              <Input type='text' className='input' placeholder='用户名/邮箱' onInput={this.changeUser} maxlength='16'></Input>
+
+            </View>
+            <View className='inputline'>
+
+              <Text className='text'>密码</Text>
+              <Input type='password' className='input' placeholder='请输入密码' onInput={this.changePwd} maxlength='16'></Input>
+
+            </View>
+            <View className='inputline'>
+            </View>
+
           </View>
 
         </View>
 
+        <View className='back'>
+          <Image className='img' src={bc}></Image>
+        </View>
+
+        <View className='footer'>
+            <View className='botom' onClick={this.login}>登录</View>
+            <View className='footer_text'>
+              <Text className='footer_text'>还没有注册?</Text>
+              <Text className='to_regist' onClick={this.toRegist}>现在注册</Text>
+            </View>
+
+          </View>
       </View>
     )
   }
--- src/pages/regist/regist.jsx
+++ src/pages/regist/regist.jsx
@@ -1,64 +1,90 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react'
-import { View, Text,Input } from '@tarojs/components'
+import { View, Text, Input } from '@tarojs/components'
 import Taro from '@tarojs/taro'
 import './regist.css'
 
 export default class Rsegist extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          user: 'dwadad',
-          pwd: 'dada'
-        }
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: '',
+      pwd: '',
+      email: '',
+      ensure: '',
+      phone: '',
+      dress: ''
+    }
+  }
 
-    toLogin = ()=>{
-        Taro.navigateTo({ url: '/pages/login/login' })
-      }
+  changeUser = (e) => {
+    // console.log(e.target.value)
+    this.setState({
+      user: e.target.value
+    })
+  }
+  changePwd = (e) => {
+    // console.log(e.target.value)
+    this.setState({
+      pwd: e.target.value
+    })
+  }
 
-      changeUser=(e)=>{
-        console.log(e)
-        this.setState({
-          user: e.target.value
-        })
-      }
-      changePwd=(e)=>{
-        console.log(e.target)
-        this.setState({
-          pwd: e.target.value
-        })
-      }
-      regist = ()=>{
-// 
-// 等待service接口
-//         
-        console.log(this.state.user)
-        console.log(this.state.pwd)
-      }
+  changeEmail = (e) => {
+    // console.log(e.target.value)
+    this.setState({
+      email: e.target.value
+    })
+  }
 
-  render () {
+  changeEnsuer = (e) => {
+    console.log(e)
+    this.setState({
+      ensure: e.detail.value
+    })
+  }
+  changePhone = (e) => {
+    // console.log(e.target.value)
+    this.setState({
+      phone: e.target.value
+    })
+  }
+  changeDress = (e) => {
+    // console.log(e.target.value)
+    this.setState({
+      dress: e.target.value
+    })
+  }
+
+  regist = () => {
+    // 
+    // 等待service接口
+    //      
+    console.log(this.state.user)
+    console.log(this.state.pwd)
+    console.log(this.state.email)
+    console.log(this.state.ensure)
+    console.log(this.state.phone)
+    console.log(this.state.dress)
+    // Taro.navigateTo({ url: '/pages/login/login' })
+  }
+
+  render() {
     return (
       <View className='index'>
-        
-        <View className='container'>
 
-          <View className='userinput'>
-            <Input className='input' placeholder='请输入账号' type='text' onInput={this.changeUser}></Input>
-          </View>
+        <View className='regist_container' >
 
-          <View className='pwdinput'>
-            <Input className='input' placeholder='请输入密码' type='password' onInput={this.changePwd}></Input>
-          </View>
-
-          <View className='botton' onClick={this.regist}>
-            <Text>确认注册</Text>
-          </View>
-          <View className='botton' onClick={this.toLogin}>
-            <Text>已有账号，返回登录</Text>
-          </View>
+          <Input className='regist_input' placeholder='用户名' maxlength='16' onInput={this.changeUser}></Input>
+          <Input className='regist_input' placeholder='邮箱' maxlength='16' onInput={this.changeEmail}></Input>
+          <Input className='regist_input' type='password' placeholder='密码' maxlength='16' onInput={this.changePwd}></Input>
+          <Input className='regist_input' type='password' placeholder='确认密码' maxlength='16' onInput={this.changeEnsuer}></Input>
+          <Input className='regist_input' placeholder='电话号码' maxlength='11' onInput={this.changePhone}></Input>
+          <Input className='regist_input' placeholder='地址' maxlength='64' onInput={this.changeDress}></Input>
+          <View className='regist_text' onClick={this.regist}>注册</View>
 
         </View>
-      
+
       </View>
     )
   }
